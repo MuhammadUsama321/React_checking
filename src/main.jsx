a0d14932c307344954ad16fc5d482c09c12a4ada
Muhammad Usama <osama2dadi95@gmail.com>
2024-12-14 08:24:01 -0800

    react

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactData from "react-dom/client";
+
+const rootDiv = document.getElementById("root");
+ReactData.createRoot(rootDiv).render(
+ 
+  <div>
+    <h1>Heloo React 5</h1>
+    <h5>Heloo React 1</h5>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas tempora dicta hic consectetur corrupti quam possimus, voluptatum soluta ad voluptas numquam fugiat, exercitationem, dolore sed! Explicabo consequuntur ex et ab! Sint doloribus velit ipsum, modi voluptas est incidunt vel laborum voluptatibus placeat itaque. Nemo tempore atque explicabo aperiam soluta quia esse ex, inventore, culpa veniam neque! Nesciunt corrupti necessitatibus fugiat autem rem deleniti accusamus ipsa tempora unde alias! Qui dolore quos, sequi molestias consequatur numquam magni, beatae eligendi similique nostrum voluptatem provident cum nihil quam corrupti sit, ducimus maiores. Eaque consequatur earum asperiores eos ducimus voluptatum harum, amet soluta nemo, nesciunt ea corrupti et perferendis non dolore quos nostrum. Pariatur voluptas quos dolorem sit impedit enim, animi illo exercitationem fugiat iste in, recusandae dignissimos omnis modi labore. Quam, nam nisi molestiae qui totam commodi pariatur dicta praesentium iure, cupiditate illo assumenda ratione nihil eum nulla! Laudantium officia fugit nemo quia dolorum cum id libero placeat in unde sint quaerat ipsum, totam quis voluptas quas tempora et aspernatur quo nam reiciendis! Iste, deleniti possimus ut minus asperiores quam tempore at, eaque velit consectetur sed ullam maiores et beatae nostrum cum voluptatibus veritatis eligendi, consequuntur dolores culpa ipsa quidem suscipit! Nulla, ducimus!
+    </p>
+    <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt="" />
+  </div>
+)
